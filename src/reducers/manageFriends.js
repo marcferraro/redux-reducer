d8739d418fff726e70169bac0c08cbe1e82dcefb
Marc <marc.ferraro1@gmail.com>
2021-03-04 13:41:55 -0600

    Add Friend Done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,4 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
 
+    switch (action.type){
+        case "ADD_FRIEND":
+            const newState = {...state, friends: [...state.friends, action.friend]}
+            return newState
+            break;
+        case "REMOVE_FRIEND":
+
+            break;
+        default:
+            return state
+    }
+    
     
 }
